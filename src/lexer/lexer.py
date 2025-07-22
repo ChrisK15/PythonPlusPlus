afd758025e91692dcc3f229d17bc0cc7a351ddde
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-07-21 20:17:04 -0700

    added basic lexer functionality

---
+++ src/lexer/lexer.py
@@ -0,0 +1,9 @@
+from typing import List
+from .token import Token, TokenType
+
+class Lexer:
+    def __init__(self, text: str):
+        self.text = text
+
+    def tokenize(self) -> List[Token]:
+        return [Token(TokenType.EOF, None, 1, 1)]
