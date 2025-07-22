88bdad3382be3c8a55421583389a14e65bfd8ed6
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-07-22 14:13:09 -0700

    updated token class in token.py to have a value of actual possible types instead of any

--- src/lexer/token.py
+++ src/lexer/token.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum, auto
+from typing import Union
 
 
 class TokenType(Enum):
@@ -10,4 +11,4 @@ class TokenType(Enum):
 @dataclass
 class Token:
     type: TokenType
-    value: any
+    value: Union[int, None]
