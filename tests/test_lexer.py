a77fedf9a3ac4c516cafa93a25ec0f4dd8921809
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-07-21 20:12:56 -0700

    created lexer.py

---
+++ tests/test_lexer.py
@@ -0,0 +1,9 @@
+from src.lexer.token import Token, TokenType
+# from src.lexer.lexer import Lexer
+
+def test_empty_input():
+    lexer = Lexer("")
+    tokens = lexer.tokenize()
+
+    assert len(tokens) == 1
+    assert tokens[0].type == TokenType.EOF
