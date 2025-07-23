14b637df5a899abd6ad07c0cdedc6d574c71e2b0
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-07-22 17:39:49 -0700

    minor refactor of lexer.py to use keywords dictionary

--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -8,6 +8,10 @@ class Lexer:
         self.text = text
         self.position = 0
 
+    KEYWORDS = {"true": (TokenType.BOOLEAN, True),
+                "false": (TokenType.BOOLEAN, False)
+    }
+
     def tokenize(self) -> List[Token]:
         tokens = []
 
@@ -15,27 +19,27 @@ class Lexer:
             if self.text[self.position].isspace():
                 self.position += 1
                 continue
+
             # If we find a digit
             if self.position < len(self.text) and self.text[self.position].isdigit():
                 current_output = ""
-                # If more digits follow
                 while self.position < len(self.text) and self.text[self.position].isdigit():
                     current_output += self.text[self.position]
                     self.position += 1
                 final_integer = int(current_output)
                 tokens.append(Token(TokenType.INTEGER, final_integer))
 
+            # If we find a word
             if self.position < len(self.text) and self.text[self.position].isalpha():
                 current_output = ""
                 while self.position < len(self.text) and self.text[self.position].isalnum():
                     current_output += self.text[self.position]
                     self.position += 1
-                if current_output == "true":
-                    final_bool = True
-                    tokens.append(Token(TokenType.BOOLEAN, final_bool))
-                elif current_output == "false":
-                    final_bool = False
-                    tokens.append(Token(TokenType.BOOLEAN, final_bool))
+                if current_output in self.KEYWORDS:
+                    token_type, token_value = self.KEYWORDS[current_output]
+                    tokens.append(Token(token_type, token_value))
+                else:
+                    tokens.append(Token(TokenType.IDENTIFIER, current_output))
 
         tokens.append(Token(TokenType.EOF, None))
         return tokens
