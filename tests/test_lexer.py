5a3db4d524c37349a2595d737bbe802e74d28ef8
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-07-22 17:47:20 -0700

    update test_lexer.py to include a test for identifiers

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -66,3 +66,14 @@ def test_boolean_false():
     assert tokens[0].type == TokenType.BOOLEAN
     assert tokens[0].value == False
     assert tokens[1].type == TokenType.EOF
+
+def test_identifier():
+    tokens = init_lexer("X Y z")
+    assert len(tokens) == 4
+    assert tokens[0].type == TokenType.IDENTIFIER
+    assert tokens[0].value == "X"
+    assert tokens[1].type == TokenType.IDENTIFIER
+    assert tokens[1].value == "Y"
+    assert tokens[2].type == TokenType.IDENTIFIER
+    assert tokens[2].value == "z"
+    assert tokens[3].type == TokenType.EOF
