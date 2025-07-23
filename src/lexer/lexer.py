4bcf61b61ca768bd261a5fc09dd1ba82b0b59a4f
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-07-22 21:55:05 -0700

    updated lexer to use imports from lexer_constants.py

--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -1,55 +1,18 @@
 from typing import List
 
 from src.lexer.token import Token, TokenType
+from src.lexer.lexer_constants import (
+    KEYWORDS,
+    SINGLE_CHAR_OPERATORS,
+    MULTI_CHAR_OPERATORS,
+    AMBIGUOUS_OPERATORS
+)
 
 
 class Lexer:
     def __init__(self, text: str):
         self.text = text
         self.position = 0
-
-    KEYWORDS = {
-        "true": (TokenType.BOOLEAN, True),
-        "false": (TokenType.BOOLEAN, False),
-        "class": (TokenType.CLASS, None),
-        "def": (TokenType.DEF, None),
-        "init": (TokenType.INIT, None),
-        "if": (TokenType.IF, None),
-        "while": (TokenType.WHILE, None),
-        "return": (TokenType.RETURN, None),
-        "break": (TokenType.BREAK, None),
-        "new": (TokenType.NEW, None),
-        "this": (TokenType.THIS, None),
-    }
-
-    OPERATORS = {"+", "-", "*", "/", "=", "!", "<", ">"}
-
-    SINGLE_CHAR_OPERATORS = {
-        "+": TokenType.PLUS,
-        "-": TokenType.MINUS,
-        "*": TokenType.MULTIPLY,
-        "/": TokenType.DIVIDE,
-        "(": TokenType.LEFT_PAREN,
-        ")": TokenType.RIGHT_PAREN,
-        "{": TokenType.LEFT_BRACE,
-        "}": TokenType.RIGHT_BRACE,
-        ";": TokenType.SEMICOLON,
-        ",": TokenType.COMMA,
-        ".": TokenType.DOT,
-        "=": TokenType.ASSIGN,
-        "<": TokenType.LESS_THAN,
-        ">": TokenType.GREATER_THAN,
-        "!": TokenType.EXCLAMATION,
-    }
-
-    MULTI_CHAR_OPERATORS = {
-        "==": TokenType.EQUAL,
-        "!=": TokenType.NOT_EQUAL,
-        "<=": TokenType.LESS_EQUAL,
-        ">=": TokenType.GREATER_EQUAL,
-    }
-
-    AMBIGUOUS_OPERATORS = {"=", "<", ">", "!"}
 
     def tokenize(self) -> List[Token]:
         tokens = []
@@ -80,8 +43,8 @@ class Lexer:
                 ):
                     current_output += self.text[self.position]
                     self.position += 1
-                if current_output in self.KEYWORDS:
-                    token_type, token_value = self.KEYWORDS[current_output]
+                if current_output in KEYWORDS:
+                    token_type, token_value = KEYWORDS[current_output]
                     tokens.append(Token(token_type, token_value))
                 else:
                     tokens.append(Token(TokenType.IDENTIFIER, current_output))
@@ -89,11 +52,11 @@ class Lexer:
             # If we find an operator
             elif (
                 self.position < len(self.text)
-                and self.text[self.position] in self.SINGLE_CHAR_OPERATORS
+                and self.text[self.position] in SINGLE_CHAR_OPERATORS
             ):
                 # The operator could consist of two characters, we have to check for this
                 if (
-                    self.text[self.position] in self.AMBIGUOUS_OPERATORS
+                    self.text[self.position] in AMBIGUOUS_OPERATORS
                     and self.position + 1 < len(self.text)
                     and self.text[self.position + 1] == "="
                 ):
@@ -101,13 +64,13 @@ class Lexer:
                         self.text[self.position] + self.text[self.position + 1]
                     )
                     tokens.append(
-                        Token(self.MULTI_CHAR_OPERATORS[current_operator], None)
+                        Token(MULTI_CHAR_OPERATORS[current_operator], None)
                     )
                     self.position += 2
                 else:
                     tokens.append(
                         Token(
-                            self.SINGLE_CHAR_OPERATORS[self.text[self.position]], None
+                            SINGLE_CHAR_OPERATORS[self.text[self.position]], None
                         )
                     )
                     self.position += 1
