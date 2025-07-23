cfd9abeb272b79d97b5a00ad575a8eefa6dc7e11
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-07-22 19:04:50 -0700

    added tests for most keywords in test_lexer.py

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -85,3 +85,55 @@ def test_identifier():
     assert tokens[2].type == TokenType.IDENTIFIER
     assert tokens[2].value == "z"
     assert tokens[3].type == TokenType.EOF
+
+
+def test_keyword_class():
+    tokens = init_lexer("class")
+    assert len(tokens) == 2
+    assert tokens[0].type == TokenType.CLASS
+    assert tokens[0].value is None
+    assert tokens[1].type == TokenType.EOF
+
+
+def test_keyword_def():
+    tokens = init_lexer("def")
+    assert len(tokens) == 2
+    assert tokens[0].type == TokenType.DEF
+    assert tokens[0].value is None
+    assert tokens[1].type == TokenType.EOF
+
+
+def test_keyword_init():
+    tokens = init_lexer("init")
+    assert len(tokens) == 2
+    assert tokens[0].type == TokenType.INIT
+    assert tokens[0].value is None
+    assert tokens[1].type == TokenType.EOF
+
+
+def test_keyword_if():
+    tokens = init_lexer("if")
+    assert len(tokens) == 2
+    assert tokens[0].type == TokenType.IF
+    assert tokens[0].value is None
+    assert tokens[1].type == TokenType.EOF
+
+
+def test_keyword_while():
+    tokens = init_lexer("while")
+    assert len(tokens) == 2
+    assert tokens[0].type == TokenType.WHILE
+    assert tokens[0].value is None
+    assert tokens[1].type == TokenType.EOF
+
+
+def test_mixed_keywords_and_identifiers():
+    tokens = init_lexer("class MyClass def myMethod")
+    assert len(tokens) == 5
+    assert tokens[0].type == TokenType.CLASS
+    assert tokens[1].type == TokenType.IDENTIFIER
+    assert tokens[1].value == "MyClass"
+    assert tokens[2].type == TokenType.DEF
+    assert tokens[3].type == TokenType.IDENTIFIER
+    assert tokens[3].value == "myMethod"
+    assert tokens[4].type == TokenType.EOF
