ee5fdce8b63eeef0dbec2dc3b0ac7e6552ece045
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-07-22 18:45:19 -0700

    added all token types according to grammar on token.py

--- src/lexer/token.py
+++ src/lexer/token.py
@@ -4,9 +4,44 @@ from typing import Union
 
 
 class TokenType(Enum):
+    # Literals
     INTEGER = auto()
     BOOLEAN = auto()
     IDENTIFIER = auto()  # var names, class names, etc.
+    
+    # Keywords
+    CLASS = auto()
+    DEF = auto()
+    INIT = auto()
+    IF = auto()
+    WHILE = auto()
+    RETURN = auto()
+    BREAK = auto()
+    NEW = auto()
+    THIS = auto()
+    
+    # Operators
+    PLUS = auto()          # +
+    MINUS = auto()         # -
+    MULTIPLY = auto()      # *
+    DIVIDE = auto()        # /
+    ASSIGN = auto()        # =
+    EQUAL = auto()         # ==
+    NOT_EQUAL = auto()     # !=
+    LESS_THAN = auto()     # <
+    LESS_EQUAL = auto()    # <=
+    GREATER_THAN = auto()  # >
+    GREATER_EQUAL = auto() # >=
+    
+    # Punctuation
+    LEFT_PAREN = auto()    # (
+    RIGHT_PAREN = auto()   # )
+    LEFT_BRACE = auto()    # {
+    RIGHT_BRACE = auto()   # }
+    SEMICOLON = auto()     # ;
+    COMMA = auto()         # ,
+    DOT = auto()           # .
+    
     EOF = auto()
 
 
