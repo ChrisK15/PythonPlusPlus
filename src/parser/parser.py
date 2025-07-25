6a9ba4e7f4be670ca837b66d0ca99c2c66a2e3e6
Chris Kelamyan <chris.kelamyan.115@gmail.com>
2025-07-25 00:01:09 -0700

    added parse_primary method in parser.py

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,3 +1,10 @@
+from src.lexer.token import TokenType
+
+from src.parser.ast_nodes import *
+
+class ParserException(Exception):
+    pass
+
 class Parser:
     def __init__(self, tokens: list):
         self.tokens = tokens
@@ -10,3 +17,26 @@ class Parser:
             self.current_token = self.tokens[self.position]
         else:
             return # Would return our results here
+
+    def parse_addition(self):
+        pass
+
+    def parse_primary(self):
+        if self.current_token.type == TokenType.INTEGER:
+            value = self.current_token.value
+            self.next_token()
+            return IntegerNode(value)
+        elif self.current_token.type == TokenType.IDENTIFIER:
+            value = self.current_token.value
+            self.next_token()
+            return IdentifierNode(value)
+        elif self.current_token.type == TokenType.LEFT_PAREN:
+            self.next_token()
+            inner_expression = self.parse_addition()
+            if self.current_token.type == TokenType.RIGHT_PAREN:
+                self.next_token()
+                return inner_expression
+            else:
+                raise ParserException(f"Error! Missing closing parenthesis.")
+        else:
+            raise ParserException(f"Error! Unknown input: {self.current_token}")
