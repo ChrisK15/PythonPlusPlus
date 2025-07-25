0a483bed2fc54ff7a6e278f4551a08d2d9edcd22
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-07-25 10:47:18 -0700

    began parse_addition method in parser.py, commiting because I need to refactor my operator tokens to have a value of their operator instead of having the value None

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -20,7 +20,19 @@ class Parser:
             return  # Would return our results here
 
     def parse_addition(self):
+        left_expression = self.parse_multiplication()
+        while self.current_token.type == TokenType.PLUS or self.current_token.type == TokenType.MINUS:
+            current_operator_token = self.current_token.type
+            self.next_token()
+            right_expression = self.parse_multiplication()
+            left_expression = BinaryOpNode(current_operator_token, left_expression, right_expression)
+        return left_expression
+
+
+
+    def parse_multiplication(self):
         pass
+
 
     def parse_primary(self):
         if self.current_token.type == TokenType.INTEGER:
