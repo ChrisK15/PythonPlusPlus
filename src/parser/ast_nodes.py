e03515d6b28bbde71a693ccd71edd8d4bc9bf895
Chris Kelamyan <chris.kelamyan.115@gmail.com>
2025-07-24 23:29:56 -0700

    added base node class and some more node types in ast_nodes.py

--- src/parser/ast_nodes.py
+++ src/parser/ast_nodes.py
@@ -1,3 +1,16 @@
-class IntegerNode:
+class Node:
+    pass
+
+class IntegerNode(Node):
     def __init__(self, value: int):
         self.value = value
+
+class IdentifierNode(Node):
+    def __init__(self, value: str):
+        self.value = value
+
+class BinaryOpNode(Node):
+    def __init__(self, op: str, left_child: Node, right_child: Node):
+        self.op = op
+        self.left_child = left_child
+        self.right_child = right_child
