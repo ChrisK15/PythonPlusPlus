cc7474f10a852cb0d11776e212b6d02230c77b43
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-07-28 09:47:21 -0700

    created parser_constants.py

---
+++ src/parser/parser_constants.py
@@ -0,0 +1,10 @@
+from src.lexer.token import TokenType
+
+COMPARISON_OPERATORS = {
+    TokenType.EQUAL,
+    TokenType.LESS_THAN,
+    TokenType.GREATER_THAN,
+    TokenType.LESS_EQUAL,
+    TokenType.GREATER_EQUAL,
+    TokenType.NOT_EQUAL
+}
