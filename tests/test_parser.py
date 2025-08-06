083bfb22b8acae007d2f1a923afeb03901a06ede
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-08-06 13:34:35 -0700

    updated nodes_equal function to assert test_output

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -39,22 +39,28 @@ def nodes_equal(test_input: Node, test_output: Node):
 
     # Expressions
     if isinstance(test_input, IntegerNode):
+        assert isinstance(test_output, IntegerNode)
         return test_input.value == test_output.value
     elif isinstance(test_input, IdentifierNode):
+        assert isinstance(test_output, IdentifierNode)
         return test_input.value == test_output.value
     elif isinstance(test_input, BooleanNode):
+        assert isinstance(test_output, BooleanNode)
         return test_input.value == test_output.value
     elif isinstance(test_input, BinaryOpNode):
+        assert isinstance(test_output, BinaryOpNode)
         return (
             test_input.op == test_output.op
             and nodes_equal(test_input.left_child, test_output.left_child)
             and nodes_equal(test_input.right_child, test_output.right_child)
         )
     elif isinstance(test_input, PrintNode):
+        assert isinstance(test_output, PrintNode)
         return nodes_equal(test_input.inner_expression, test_output.inner_expression)
     elif isinstance(test_input, ThisNode):
         return True  # ThisNode has no attributes to compare
     elif isinstance(test_input, NewNode):
+        assert isinstance(test_output, NewNode)
         if test_input.class_name != test_output.class_name:
             return False
         if len(test_input.arguments) != len(test_output.arguments):
@@ -64,6 +70,7 @@ def nodes_equal(test_input: Node, test_output: Node):
                 return False
         return True
     elif isinstance(test_input, CallNode):
+        assert isinstance(test_output, CallNode)
         if test_input.method_name != test_output.method_name:
             return False
         if not nodes_equal(test_input.obj_node, test_output.obj_node):
@@ -77,24 +84,29 @@ def nodes_equal(test_input: Node, test_output: Node):
 
     # Statements
     elif isinstance(test_input, ExpressionStatement):
+        assert isinstance(test_output, ExpressionStatement)
         return nodes_equal(test_input.exp, test_output.exp)
     elif isinstance(test_input, VarDecStatement):
+        assert isinstance(test_output, VarDecStatement)
         return (
             test_input.var_type == test_output.var_type
             and test_input.var == test_output.var
             and nodes_equal(test_input.val, test_output.val)
         )
     elif isinstance(test_input, AssignmentStatement):
+        assert isinstance(test_output, AssignmentStatement)
         return test_input.var == test_output.var and nodes_equal(
             test_input.exp, test_output.exp
         )
     elif isinstance(test_input, WhileStatement):
+        assert isinstance(test_output, WhileStatement)
         return nodes_equal(test_input.exp, test_output.exp) and nodes_equal(
             test_input.stmt, test_output.stmt
         )
     elif isinstance(test_input, BreakStatement):
         return True
     elif isinstance(test_input, ReturnStatement):
+        assert isinstance(test_output, ReturnStatement)
         if test_input.exp is None and test_output.exp is None:
             return True
         elif test_input.exp is None or test_output.exp is None:
@@ -102,6 +114,7 @@ def nodes_equal(test_input: Node, test_output: Node):
         else:
             return nodes_equal(test_input.exp, test_output.exp)
     elif isinstance(test_input, IfStatement):
+        assert isinstance(test_output, IfStatement)
         if test_input.else_stmt is None and test_output.else_stmt is None:
             return nodes_equal(test_input.exp, test_output.exp) and nodes_equal(
                 test_input.then_stmt, test_output.then_stmt
@@ -115,6 +128,7 @@ def nodes_equal(test_input: Node, test_output: Node):
                 and nodes_equal(test_input.else_stmt, test_output.else_stmt)
             )
     elif isinstance(test_input, BlockStatement):
+        assert isinstance(test_output, BlockStatement)
         if len(test_input.stmts) == len(test_output.stmts):
             for input_stmt, output_stmt in zip(test_input.stmts, test_output.stmts):
                 if not nodes_equal(input_stmt, output_stmt):
@@ -124,6 +138,7 @@ def nodes_equal(test_input: Node, test_output: Node):
 
     # Declarations
     elif isinstance(test_input, MethodDef):
+        assert isinstance(test_output, MethodDef)
         if (
             test_input.method_type != test_output.method_type
             or test_input.method_name != test_output.method_name
@@ -147,6 +162,7 @@ def nodes_equal(test_input: Node, test_output: Node):
         else:
             return False
     elif isinstance(test_input, Constructor):
+        assert isinstance(test_output, Constructor)
         if len(test_input.parameters) == len(test_output.parameters):
             for input_param, output_param in zip(
                 test_input.parameters, test_output.parameters
