1a9e747c5c5ce7de6997e16329c59d0b349deb2d
ChrisK15 <chris.kelamyan.115@gmail.com>
2025-08-11 20:36:28 -0700

    cli commands implemented

--- src/compiler.py
+++ src/compiler.py
@@ -1,2 +1,11 @@
+import sys
+from src.lexer.lexer import Lexer
+from src.parser.parser import Parser
+from src.code_generator.code_generator import CodeGenerator
+
 def main():
-    print("Hello world")
+    if len(sys.argv) < 2:
+        print("Usage: ppp <input.pp>")
+        return
+    input_file = sys.argv[1]
+    print(f"Compiling: {input_file}")
